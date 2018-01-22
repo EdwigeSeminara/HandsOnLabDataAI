f0b96a2ad7dbf3004d63cb04e97e0064a0772468
Edwige Seminara <edwige.seminara@live.com>
2018-01-23 00:48:02 +0100

    Solution add

---
+++ 4-Chatbot/SmartFactoryBot/SmartFactoryBot/Checklist.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace SmartFactoryBot
+{
+    [Serializable]
+    [Template(TemplateUsage.NotUnderstood,
+        "Sorry, I didn't get that.",
+        "Please try again",
+        "My apologies, I didn't understand \"{0}\".",
+        "I don't understand \"{0}\".",
+        "Excuse me but Im' a chatbot and don't know \"{0}\".",
+        "Try again, I don't get \"{0}\".")]
+    public class Checklist
+    {
+        public string TechnicianId { get; set; }
+
+        private static ConcurrentDictionary<CultureInfo, IForm<Checklist>> _forms = new ConcurrentDictionary<CultureInfo, IForm<Checklist>>();
+
+        public static IForm<Checklist> BuildForm()
+        {
+            var culture = Thread.CurrentThread.CurrentUICulture;
+            IForm<Checklist> form;
+
+            if (!_forms.TryGetValue(culture, out form))
+            {
+                OnCompletionAsyncDelegate<Checklist> processChecklist = async (context, state) =>
+                {
+                    await context.PostAsync("We are currently processing your checklist. We will message you the status.");
+                };
+
+                var formBuilder = new FormBuilder<Checklist>();
+                formBuilder
+                        .Message("Hello, my name is Chuck the checkbot !")
+                        .Field(nameof(TechnicianId))
+                        .Message("Thank you");
+
+                formBuilder.Configuration.DefaultPrompt.ChoiceStyle = ChoiceStyleOptions.Auto;
+                form = formBuilder.Build();
+                _forms[culture] = form;
+            }
+            return form;
+        }
+
+        
+    }
+}
---
+++ 4-Chatbot/SmartFactoryBot/SmartFactoryBot/Controllers/MessagesController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Connector;
+
+namespace SmartFactoryBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        [ResponseType(typeof(void))]
+        public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
+        {
+            if (activity != null)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+                // one of these will have an interface and process it
+                switch (activity.GetActivityType())
+                {
+                    case ActivityTypes.Message:
+                        await Conversation.SendAsync(activity, MakeRootDialog);
+                        break;
+
+                    case ActivityTypes.ConversationUpdate:
+                    case ActivityTypes.ContactRelationUpdate:
+                    case ActivityTypes.Typing:
+                    case ActivityTypes.DeleteUserData:
+                    default:
+                        Trace.TraceError($"Unknown activity type ignored: {activity.GetActivityType()}");
+                        break;
+                }
+            }
+            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+        }
+
+        internal static IDialog<Checklist> MakeRootDialog()
+        {
+            return Chain.From(() => FormDialog.FromForm(Checklist.BuildForm))
+            .Do(async (context, order) =>
+            {
+                try
+                {
+                    var completed = await order;
+                    // Actually process the coffee order...
+                    await context.PostAsync("Checklist verified !");
+                }
+                catch (FormCanceledException<Checklist> e)
+                {
+                    string reply;
+                    if (e.InnerException == null)
+                    {
+                        reply = $"You quit on {e.Last}--maybe you can finish next time!";
+                    }
+                    else
+                    {
+                        reply = "Sorry, I've had a short circuit.  Please try again.";
+                    }
+                    await context.PostAsync(reply);
+                }
+            });
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+
+}
